86e408540bb2ec69508044e72548b4d250c92764
Will Harris <will.t.harris@protonmail.com>
2022-12-24 00:24:34 -0800

    add apollo sandbox to http server

--- main.go
+++ main.go
@@ -7,13 +7,35 @@ import (
 )
 
 func main() {
-	h := handler.New(&handler.Config{
+	handler := handler.New(&handler.Config{
 		Schema:   &BeastSchema,
 		Pretty:   true,
-		GraphiQL: true,
+		GraphiQL: false,
 	})
 
-	http.Handle("/graphql", h)
+	http.Handle("/graphql", handler)
+
+	http.Handle("/sandbox", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write(sandboxHTML)
+	}))
 
 	http.ListenAndServe(":8080", nil)
 }
+
+var sandboxHTML = []byte(`
+<!DOCTYPE html>
+<html lang="en">
+<body style="margin: 0; overflow-x: hidden; overflow-y: hidden">
+<div id="sandbox" style="height:100vh; width:100vw;"></div>
+<script src="https://embeddable-sandbox.cdn.apollographql.com/_latest/embeddable-sandbox.umd.production.min.js"></script>
+<script>
+ new window.EmbeddedSandbox({
+   target: "#sandbox",
+   // Pass through your server href if you are embedding on an endpoint.
+   // Otherwise, you can pass whatever endpoint you want Sandbox to start up with here.
+   initialEndpoint: "http://localhost:8080/graphql",
+ });
+ // advanced options: https://www.apollographql.com/docs/studio/explorer/sandbox#embedding-sandbox
+</script>
+</body>
+</html>`)
