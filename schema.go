5dacf82d2ddb4ffcdcad2f3fedb46d697fdae2d0
Will Harris <will.t.harris@protonmail.com>
2022-12-24 00:24:21 -0800

    add initial mutations

--- schema.go
+++ schema.go
@@ -99,6 +99,107 @@ var beastType = graphql.NewObject(graphql.ObjectConfig{
 	},
 })
 
+var currentMaxId = 5
+
+var rootMutation = graphql.NewObject(graphql.ObjectConfig{
+	Name: "RootMutation",
+	Fields: graphql.Fields{
+		"addBeast": &graphql.Field{
+			Type:        beastType,
+			Description: "Add a new beast",
+			Args: graphql.FieldConfigArgument{
+				"name": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+				"description": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+				"otherNames": &graphql.ArgumentConfig{
+					Type: graphql.NewList(graphql.String),
+				},
+				"imageUrl": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				name, _ := params.Args["name"].(string)
+				description, _ := params.Args["description"].(string)
+				otherNames, _ := params.Args["otherNames"].([]string)
+				imageUrl, _ := params.Args["imageUrl"].(string)
+
+				newId := currentMaxId + 1
+				currentMaxId = currentMaxId + 1
+
+				newBeast := Beast{
+					Id:          newId,
+					Name:        name,
+					Description: description,
+					OtherNames:  otherNames,
+					ImageUrl:    imageUrl,
+				}
+
+				BeastList = append(BeastList, newBeast)
+
+				// We would save the new beast to db here
+
+				return newBeast, nil
+			},
+		},
+
+		"updateBeast": &graphql.Field{
+			Type:        beastType,
+			Description: "Update existing beast",
+			Args: graphql.FieldConfigArgument{
+				"name": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"description": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.Int),
+				},
+				"otherNames": &graphql.ArgumentConfig{
+					Type: graphql.NewList(graphql.String),
+				},
+				"imageUrl": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				id, _ := params.Args["id"].(int)
+				updatedBeast := Beast{}
+
+				for i := 0; i < len(BeastList); i++ {
+					if BeastList[i].Id == id {
+						if _, ok := params.Args["description"]; ok {
+							BeastList[i].Description = params.Args["description"].(string)
+						}
+
+						if _, ok := params.Args["name"]; ok {
+							BeastList[i].Name = params.Args["name"].(string)
+						}
+
+						if _, ok := params.Args["imageUrl"]; ok {
+							BeastList[i].ImageUrl = params.Args["imageUrl"].(string)
+						}
+
+						if _, ok := params.Args["otherNames"]; ok {
+							BeastList[i].OtherNames = params.Args["otherNames"].([]string)
+						}
+
+						updatedBeast = BeastList[i]
+						break
+					}
+				}
+
+				return updatedBeast, nil
+			},
+		},
+	},
+})
+
 var BeastSchema, _ = graphql.NewSchema(graphql.SchemaConfig{
-	Query: rootQuery,
+	Query:    rootQuery,
+	Mutation: rootMutation,
 })
